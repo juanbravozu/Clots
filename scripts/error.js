72e793b4e82cbdddb0e1503f77abbd457f16cbca
Juan David Bravo <juanda.bravo98@gmail.com>
2020-12-04 13:34:24 -0500

    error pages coded

---
+++ scripts/error.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => {
+
+    const btn = document.querySelector('button');
+
+    btn.addEventListener('click', () => {
+        window.location.href = '/index.html';
+    });
+
+});
