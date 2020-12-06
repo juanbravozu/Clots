c8793fa6c8f4d03a61bb7fa0f02363245e365bb9
Juan David Bravo <juanda.bravo98@gmail.com>
2020-12-05 22:35:41 -0500

    Made modal responsive

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,9 @@ window.addEventListener('load', () => {
     }
 
     const openModalBtns = document.querySelectorAll('.modalBtn');
+    const closeModalBtn = document.querySelector('.modal div img');
 
     openModalBtns.forEach(btn => btn.addEventListener('click', openModal));
+
+    closeModalBtn.addEventListener('click', (e) => e.target.closest('.modal').classList.add('modal--inactive'));
 });
